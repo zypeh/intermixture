aa7d769ae15bc7a0ea782602fc27b85708c714ef
pehcy <cheeyung.peh@gmail.com>
2021-02-08 23:40:34 +0800

    refactor: using airbnb style

--- src/pages/Notebook/Toolbar.tsx
+++ src/pages/Notebook/Toolbar.tsx
@@ -1,14 +1,23 @@
 import ICONS from '../../Icons';
 
+function onMouseUp(event: React.MouseEvent, name: string) {
+  const selection = window.getSelection();
+  console.log(selection);
+}
+
 const Toolbar = () => (
   <div className="flex flex-row">
-    { (Object.keys(ICONS) as Array<keyof typeof ICONS>).map(item => (
-        <div className="ml-5" role="button" key={ item }>
+    { (Object.keys(ICONS) as Array<keyof typeof ICONS>).map((item) => (
+        <div className="ml-5" 
+          role="button" 
+          key={item} 
+          onClick={(e) => onMouseUp(e, item)}
+        >
           <svg viewBox="0 0 16 16" width={16} height={16}>
             <path
               fillRule="evenodd"
-              d={ ICONS[item].path }
-            ></path>
+              d={ICONS[item].path}
+            />
           </svg>
         </div>
     ))}
